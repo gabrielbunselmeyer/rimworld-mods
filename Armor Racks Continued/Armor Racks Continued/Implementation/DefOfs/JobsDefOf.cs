e484225c82ce84392bb6d97e3deeb523a8fc55c8
Gabriel Bunselmeyer <gabrielbunselmeyer@gmail.com>
2021-05-17 23:46:28 -0300

    Add job DefOfs

---
+++ Armor Racks Continued/Armor Racks Continued/Implementation/DefOfs/JobsDefOf.cs
@@ -0,0 +1,12 @@
+using RimWorld;
+using Verse;
+
+namespace ArmorRacksContinued.Implementation.DefOfs
+{
+    [DefOf]
+    internal static class ArmorRacksJobDefOf
+    {
+        public static JobDef ArmorRacks_JobWearFromRack;
+        public static JobDef ArmorRacks_JobTransferToRack;
+    }
+}
